f678ca976e8d92eb14e075e6da16064bd08a30c5
Artur Rios <arturdev@duck.com>
2025-04-09 19:50:21 -0300

    refactor: nullable return type on GetById method

--- src/TechCraftsmen.Core.Data/ICrudRepository.cs
+++ src/TechCraftsmen.Core.Data/ICrudRepository.cs
@@ -4,7 +4,7 @@ public interface ICrudRepository<T> where T : Entity
 {
     int Create(T entity);
     IQueryable<T> GetByFilter(DataFilter filter);
-    T GetById(int id);
+    T? GetById(int id);
     void Update(T entity);
     void Delete(int id);
 }
