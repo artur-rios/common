ddc113b14277c656739423aab1adb5c63045012c
Artur Rios <arturdev@duck.com>
2025-04-09 18:50:18 -0300

    feat: add credentials validator

---
+++ src/TechCraftsmen.Core.WebApi/Security/Validation/CredentialsValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using TechCraftsmen.Core.WebApi.Security.Records;
+
+namespace TechCraftsmen.Core.WebApi.Security.Validation;
+
+public class CredentialsValidator : AbstractValidator<Credentials>
+{
+    public CredentialsValidator()
+    {
+        RuleFor(authCredentials => authCredentials.Email).NotEmpty();
+        RuleFor(authCredentials => authCredentials.Password).NotEmpty();
+    }
+}
