0b6202e891df9016d13dcaafbe3c22fe1e107963
Artur Rios <arturdev@duck.com>
2025-04-09 19:05:11 -0300

    feat: add fluent validations extension

---
+++ src/TechCraftsmen.Core.Extensions/FluentValidationExtensions.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation.Results;
+using TechCraftsmen.Core.Output;
+
+namespace TechCraftsmen.Core.Extensions;
+
+public static class FluentValidationExtensions
+{
+    public static DataOutput<T> ToDataOutput<T>(this ValidationResult validationResult)
+    {
+        var validationErrors = validationResult.Errors.Select(vf => vf.ErrorMessage).ToArray();
+
+        return new DataOutput<T>(default, validationErrors, validationResult.IsValid);
+    }
+    
+    public static ProcessOutput ToProcessOutput(this ValidationResult validationResult)
+    {
+        var validationErrors = validationResult.Errors.Select(vf => vf.ErrorMessage).ToArray();
+
+        return new ProcessOutput(validationErrors);
+    }
+}
