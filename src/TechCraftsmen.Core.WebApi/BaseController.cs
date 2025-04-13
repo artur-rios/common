cd5bb8026bcf95dbe975f7312f8fff419c3099c1
Artur Rios <arturdev@duck.com>
2025-04-13 14:45:18 -0300

    feat: add resolve method on base controller to deal with data outputs

--- src/TechCraftsmen.Core.WebApi/BaseController.cs
+++ src/TechCraftsmen.Core.WebApi/BaseController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TechCraftsmen.Core.Output;
 
 namespace TechCraftsmen.Core.WebApi;
 
@@ -6,6 +7,14 @@ public class BaseController : Controller
 {
     public static ActionResult<WebApiOutput<T>> Resolve<T>(WebApiOutput<T> webApiOutput)
     {
+        return webApiOutput.ToObjectResult();
+    }
+    
+    public static ActionResult<WebApiOutput<T>> Resolve<T>(DataOutput<T> dataOutput)
+    {
+        var statusCode = dataOutput.Success ? HttpStatusCodes.Ok : HttpStatusCodes.BadRequest;
+        var webApiOutput = new WebApiOutput<T>(dataOutput, statusCode);
+        
         return webApiOutput.ToObjectResult();
     }
 }
