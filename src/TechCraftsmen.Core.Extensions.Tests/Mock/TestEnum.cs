23d88b2092e1af2f72751703da57dfed4d1b9ef5
Artur Rios <arturdev@duck.com>
2025-04-15 00:16:21 -0300

    feat: add enum extensions

---
+++ src/TechCraftsmen.Core.Extensions.Tests/Mock/TestEnum.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace TechCraftsmen.Core.Extensions.Tests.Mock;
+
+public enum TestEnum
+{
+    [Description("One")]
+    One = 1,
+    
+    [Description("Two")]
+    Two = 2,
+    
+    Three = 3
+}
---
+++ src/TechCraftsmen.Core.Extensions/EnumExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace TechCraftsmen.Core.Extensions;
+
+public static class EnumExtensions
+{
+    public static string? GetDescription(this Enum value)
+    {
+        var field = value.GetType().GetField(value.ToString());
+        var attribute = field?.GetCustomAttributes(typeof(DescriptionAttribute), false)
+            .Cast<DescriptionAttribute>()
+            .FirstOrDefault();
+        
+        return attribute?.Description;
+    }
+}
