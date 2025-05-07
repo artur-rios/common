4e59be3b6a9ed9bfdb0a45da6afc848e5bf70d48
Artur Rios <arturdev@duck.com>
2025-05-07 15:48:23 -0300

    feat: add special and all chars collections

--- src/TechCraftsmen.Core.Validation/CharacterCollection.cs
+++ src/TechCraftsmen.Core.Validation/CharacterCollection.cs
@@ -5,4 +5,6 @@ public class CharacterCollection
     public const string Digits = "0123456789";
     public const string LowerChars = "abcdefghijklmnopqrstuvwxyz";
     public const string UpperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public const string SpecialChars = "!@#$%^&*()_+-=[]{}|;':\",.<>?/";
+    public const string AllChars = Digits + LowerChars + UpperChars + SpecialChars;
 }
