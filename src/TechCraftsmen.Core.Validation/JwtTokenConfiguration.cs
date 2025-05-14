38818dc0f3e3a2ea43d893dec12b5a1533a87f12
Artur Rios <arturdev@duck.com>
2025-05-14 18:46:45 -0300

    fix: add parameterless constructor on JwtTokenConfiguration record

--- src/TechCraftsmen.Core.Validation/JwtTokenConfiguration.cs
+++ src/TechCraftsmen.Core.Validation/JwtTokenConfiguration.cs
@@ -1,3 +1,7 @@
 ﻿namespace TechCraftsmen.Core.Validation;
 
-public record JwtTokenConfiguration(double ExpirationInSeconds, string Issuer, string Audience, string Secret);
+public record JwtTokenConfiguration(double ExpirationInSeconds, string Issuer, string Audience, string Secret)
+{
+    // Necessary for dynamic dependency injection
+    public JwtTokenConfiguration() : this(0, string.Empty, string.Empty, string.Empty) { }
+}
