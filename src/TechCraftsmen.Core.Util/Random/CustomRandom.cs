aa3864c859b4f29b27e1348bfbfc28d3baa6119f
Artur Rios <arturdev@duck.com>
2025-05-15 16:39:20 -0300

    feat: add class to generate random numbers and text

---
+++ src/TechCraftsmen.Core.Util/Random/CustomRandom.cs
@@ -0,0 +1,106 @@
+﻿// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedType.Global
+// Reason: This class in meant to be used in other projects
+
+using System.Security.Cryptography;
+using System.Text;
+using TechCraftsmen.Core.Util.Collections;
+
+namespace TechCraftsmen.Core.Util.Random;
+
+public class CustomRandom
+{
+    public static int NumberFromRng(int start, int end, int? differentFrom = null)
+    {
+        end++;
+
+        var random = RandomNumberGenerator.GetInt32(start, end);
+
+        if (differentFrom is null)
+        {
+            return random;
+        }
+
+        while (random == differentFrom)
+        {
+            random = RandomNumberGenerator.GetInt32(start, end);
+        }
+
+        return random;
+    }
+
+
+    public static int NumberFromSystemRandom(int start, int end, int? differentFrom = null)
+    {
+        System.Random rng = new();
+
+        var random = rng.Next(start, end);
+
+        if (differentFrom is null)
+        {
+            return random;
+        }
+
+        while (random == differentFrom)
+        {
+            random = rng.Next(start, end);
+        }
+        
+        return random;
+    }
+    
+    public static string Text(RandomStringOptions options, string[]? differentFrom = null)
+    {
+        while (true)
+        {
+            var random = new System.Random();
+            var password = new StringBuilder();
+
+            var charCollectionCount = 0;
+
+            if (options.IncludeLowercase)
+            {
+                password.Append(CharacterCollection.LowerChars[random.Next(CharacterCollection.LowerChars.Length)]);
+                charCollectionCount++;
+            }
+
+            if (options.IncludeUppercase)
+            {
+                password.Append(CharacterCollection.UpperChars[random.Next(CharacterCollection.UpperChars.Length)]);
+                charCollectionCount++;
+            }
+
+            if (options.IncludeSpecialCharacters)
+            {
+                password.Append(CharacterCollection.SpecialChars[random.Next(CharacterCollection.SpecialChars.Length)]);
+                charCollectionCount++;
+            }
+
+            if (options.IncludeDigits)
+            {
+                password.Append(CharacterCollection.Digits[random.Next(CharacterCollection.Digits.Length)]);
+                charCollectionCount++;
+            }
+
+            for (var i = charCollectionCount; i < options.Length; i++)
+            {
+                password.Append(CharacterCollection.AllChars[random.Next(CharacterCollection.AllChars.Length)]);
+            }
+
+            var generatedString = new string(password.ToString().OrderBy(_ => random.Next()).ToArray());
+            var matchesExcludedString = false;
+
+            if (differentFrom != null)
+            {
+                matchesExcludedString = differentFrom.Any(excludedString => excludedString.Equals(generatedString));
+            }
+
+            if (matchesExcludedString)
+            {
+                continue;
+            }
+
+            return generatedString;
+        }
+    }
+}
