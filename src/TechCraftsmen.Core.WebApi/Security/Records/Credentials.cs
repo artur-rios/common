67efc635410d84d44dff18c65ec02a8e001813ee
Artur Rios <arturdev@duck.com>
2025-05-15 23:43:39 -0300

    chore: add parameterless constructor on credentials record

--- src/TechCraftsmen.Core.WebApi/Security/Records/Credentials.cs
+++ src/TechCraftsmen.Core.WebApi/Security/Records/Credentials.cs
@@ -2,4 +2,9 @@
 
 // ReSharper disable once ClassNeverInstantiated.Global
 // Reason: This record is meant to be used in other projects
-public record Credentials(string Email, string Password);
+public record Credentials(string Email, string Password)
+{
+    // ReSharper disable once UnusedMember.Global
+    // Reason: Necessary for dynamic dependency injection
+    public Credentials() : this(string.Empty, string.Empty) { }
+}
