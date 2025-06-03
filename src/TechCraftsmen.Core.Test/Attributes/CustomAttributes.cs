0bcd824476da53fc6c46b6caa4e8feb270791ca1
Artur Rios <arturdev@duck.com>
2025-06-03 18:26:09 -0300

    refactor: change test attribute names to be more descriptive

--- src/TechCraftsmen.Core.Test/Attributes/CustomAttributes.cs
+++ src/TechCraftsmen.Core.Test/Attributes/CustomAttributes.cs
@@ -5,10 +5,10 @@ using TechCraftsmen.Core.Environment;
 
 namespace TechCraftsmen.Core.Test.Attributes;
 
-public class UnitAttribute(EnvironmentType[]? environments = null) : CustomFactAttribute(environments);
+public class UnitFactAttribute(EnvironmentType[]? environments = null) : CustomFactAttribute(environments);
 
 public class UnitTheoryAttribute(EnvironmentType[]? environments = null) : CustomTheoryAttribute(environments);
 
-public class FunctionalAttribute(EnvironmentType[]? environments = null) : CustomFactAttribute(environments);
+public class FunctionalFactAttribute(EnvironmentType[]? environments = null) : CustomFactAttribute(environments);
 
 public class FunctionalTheoryAttribute(EnvironmentType[]? environments = null) : CustomTheoryAttribute(environments);
