80721283de71526cbb1080a23e551763ada934cc
Artur Rios <arturdev@duck.com>
2025-06-18 17:07:00 -0300

    feat: add interface for repositories with operation with multiple entities at once

---
+++ src/TechCraftsmen.Core.Data/IMultiRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace TechCraftsmen.Core.Data;
+
+public interface IMultiRepository<out T> where T : Entity
+{
+    IQueryable<T> GetByMultiFilter(DataFilter filter, bool track = false);
+    void MultiDelete(IEnumerable<int> ids);
+}
