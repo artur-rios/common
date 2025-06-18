e305689b8d471bdea5344e085220139b18c82f2d
Artur Rios <arturdev@duck.com>
2025-06-17 23:27:38 -0300

    feat: add default message on process output map to webapi output

--- src/TechCraftsmen.Core.WebApi/WebApiOutput.cs
+++ src/TechCraftsmen.Core.WebApi/WebApiOutput.cs
@@ -12,32 +12,35 @@ public class WebApiOutput<T> : DataOutput<T>
     public WebApiOutput()
     {
     }
-    
+
     public WebApiOutput(T? data, string[] messages, bool success, int httpStatusCode) : base(data, messages, success)
     {
         ValidateStatusCode(httpStatusCode);
-        
+
         _httpStatusCode = httpStatusCode;
     }
 
-    public WebApiOutput(DataOutput<T> dataOutput, int httpStatusCode) : base(dataOutput.Data, dataOutput.Messages, dataOutput.Success)
+    public WebApiOutput(DataOutput<T> dataOutput, int httpStatusCode) : base(dataOutput.Data, dataOutput.Messages,
+        dataOutput.Success)
     {
         ValidateStatusCode(httpStatusCode);
-        
+
         _httpStatusCode = httpStatusCode;
     }
-    
-    public WebApiOutput(ProcessOutput processOutput, T dataOutput, int httpStatusCode) : base(dataOutput, processOutput.Errors.ToArray(), processOutput.Success)
+
+    public WebApiOutput(ProcessOutput processOutput, T dataOutput, int httpStatusCode) : base(dataOutput,
+        processOutput.Errors.Count > 0 ? processOutput.Errors.ToArray() : ["Process executed with no errors"],
+        processOutput.Success)
     {
         ValidateStatusCode(httpStatusCode);
-        
+
         _httpStatusCode = httpStatusCode;
     }
 
 
     public ObjectResult ToObjectResult()
     {
-        return new ObjectResult(this){ StatusCode = _httpStatusCode };
+        return new ObjectResult(this) { StatusCode = _httpStatusCode };
     }
 
     private static void ValidateStatusCode(int httpStatusCode)
