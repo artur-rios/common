0f35ce980612d680a06a127e9fe24bbaf0275c53
Artur Rios <arturdev@duck.com>
2025-07-23 19:54:09 -0300

    fix: converter method

--- src/ArturRios.Common.Data/ValueConverterCollection.cs
+++ src/ArturRios.Common.Data/ValueConverterCollection.cs
@@ -1,19 +1,21 @@
 ﻿// ReSharper disable InconsistentNaming
 // Reason: these are not test methods
 
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedType.Global
+// Reason: these methods are meant to be used in other projects
+
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ArturRios.Common.Data;
 
 public static class ValueConverterCollection
 {
-    public static ValueConverter<T, string> EnumIntToString<T>() where T : Enum
+    public static ValueConverter<int, string> EnumIntToString<T>() where T : Enum
     {
-        var converter = new ValueConverter<T, string>(
-            v => v.ToString(),
-            v => (T)Enum.Parse(typeof(T), v)
+        return new ValueConverter<int, string>(
+            v => Enum.GetName(typeof(T), v) ?? string.Empty,
+            v => (int)Enum.Parse(typeof(T), v)
         );
-
-        return converter;
     }
 }
