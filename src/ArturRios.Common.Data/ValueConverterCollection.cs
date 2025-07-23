f377a3735e485e4f20162dca870dd6378fcbc494
Artur Rios <arturdev@duck.com>
2025-07-23 18:20:33 -0300

    feat: add value converter collection

---
+++ src/ArturRios.Common.Data/ValueConverterCollection.cs
@@ -0,0 +1,19 @@
+﻿// ReSharper disable InconsistentNaming
+// Reason: these are not test methods
+
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace ArturRios.Common.Data;
+
+public static class ValueConverterCollection
+{
+    public static ValueConverter<T, string> EnumIntToString<T>() where T : Enum
+    {
+        var converter = new ValueConverter<T, string>(
+            v => v.ToString(),
+            v => (T)Enum.Parse(typeof(T), v)
+        );
+
+        return converter;
+    }
+}
