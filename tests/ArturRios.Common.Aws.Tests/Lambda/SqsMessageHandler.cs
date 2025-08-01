fd0bfa357e8fc9f8b36173ed6495484e7894a067
Artur Rios <arturdev@duck.com>
2025-08-01 12:16:47 -0300

    refactor: move handler to test project

--- tests/ArturRios.Common.Aws.Tests/Lambda/SqsMessageHandler.cs
+++ tests/ArturRios.Common.Aws.Tests/Lambda/SqsMessageHandler.cs
@@ -1,10 +1,11 @@
 ﻿using Amazon.Lambda.SQSEvents;
+using ArturRios.Common.Aws.Sqs;
 using ArturRios.Common.Extensions;
 using ArturRios.Common.Output;
 using ArturRios.Common.Pipelines;
 using Microsoft.Extensions.Logging;
 
-namespace ArturRios.Common.Aws.Sqs;
+namespace ArturRios.Common.Aws.Tests.Lambda;
 
 public class SqsMessageHandler(ICommandPipeline pipeline, ILogger<SqsMessageHandler> logger) : ISqsMessageHandler
 {
@@ -23,15 +24,16 @@ public class SqsMessageHandler(ICommandPipeline pipeline, ILogger<SqsMessageHand
 
             var output = await pipeline.ExecuteCommand(commandInput);
 
-            if (!output.Success)
+            if (output.Success)
             {
-                logger.LogError("The command {CommandType} | Id: {CommandId} failed with errors: {Errors}",
-                    commandInput.TypeFullName, commandInput.CommandId, StringArrayExtensions.JoinWith(output.Messages));
-
-                return new ProcessOutput { Errors = Enumerable.ToList<string>(output.Messages) };
+                return new ProcessOutput();
             }
 
-            return new ProcessOutput();
+            logger.LogError("The command {CommandType} | Id: {CommandId} failed with errors: {Errors}",
+                commandInput.TypeFullName, commandInput.CommandId, output.Messages.JoinWith());
+
+            return new ProcessOutput { Errors = output.Messages.ToList() };
+
         }
         catch (Exception ex)
         {
