32a6cdd59cd346f767500af2c67b1658f9cd261c
Artur Rios <arturdev@duck.com>
2025-09-09 00:39:42 -0300

    refactor: remove SetRepositories method

--- src/ArturRios.Common.Data/BaseDataClient.cs
+++ src/ArturRios.Common.Data/BaseDataClient.cs
@@ -1,6 +1,4 @@
-﻿// ReSharper disable VirtualMemberCallInConstructor
-// Reason: This is intentional to allow derived classes to set up repositories during construction
-using ArturRios.Common.Configuration.Enums;
+﻿using ArturRios.Common.Configuration.Enums;
 using ArturRios.Common.Configuration.Loaders;
 
 namespace ArturRios.Common.Data;
@@ -11,16 +9,10 @@ public abstract class BaseDataClient
     {
         var configLoader = new ConfigurationLoader(environment.ToString());
         configLoader.LoadEnvironment();
-
-        SetRepositories();
     }
 
     protected BaseDataClient(string connectionString)
     {
         Environment.SetEnvironmentVariable("DATABASE_CONNECTION_STRING", connectionString);
-
-        SetRepositories();
     }
-
-    protected abstract void SetRepositories();
 }
