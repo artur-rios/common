954b39162790753b4476f0d9062f81211d51c8bf
Artur Rios <arturdev@duck.com>
2025-09-29 21:58:51 -0300

    feat: add column order annotation on entity id

--- src/ArturRios.Common.Data/Entity.cs
+++ src/ArturRios.Common.Data/Entity.cs
@@ -1,9 +1,13 @@
 ﻿// ReSharper disable UnusedMember.Global
 // Reason: This class is meant to be used in other projects
 
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ArturRios.Common.Data;
 
 public abstract class Entity
 {
+
+    [Column(Order = 1)]
     public int Id { get; set; }
 }
