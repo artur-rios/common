532beecc5ccbfdd5ba8b618dfcfe76d93ac96649
Artur Rios <arturdev@duck.com>
2025-10-20 19:10:02 -0300

    feat: add extension method to remove milliseconds from datetime

---
+++ src/ArturRios.Common.Extensions/DateTimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace ArturRios.Common.Extensions;
+
+public static class DateTimeExtensions
+{
+    public static DateTime RemoveMilliseconds(this DateTime dateTime)
+    {
+        return new DateTime(
+            dateTime.Year,
+            dateTime.Month,
+            dateTime.Day,
+            dateTime.Hour,
+            dateTime.Minute,
+            dateTime.Second,
+            dateTime.Kind);
+    }
+}
